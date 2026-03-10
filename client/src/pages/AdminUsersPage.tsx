fe1830bd806f7d1220deb3b981f6a0c80321be52
menahemerlich <erlichmenahem@gmail.com>
2026-03-10 12:41:30 +0200

    finish server

--- client/src/pages/AdminUsersPage.tsx
+++ client/src/pages/AdminUsersPage.tsx
@@ -1,8 +1,21 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { AuthContext } from '../AuthContext'
 
 function AdminUsersPage() {
     const { token } = useContext(AuthContext)
+    const [data, setData] = useState([])
+    async function fetchUsers() {
+        const response = await fetch(
+            `http://localhost:3001/admin/users`,
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            }
+        )
+        const result = await response.json()
+        setData(result)
+    }
   return (
     <div>
         <div>Create new user</div>
